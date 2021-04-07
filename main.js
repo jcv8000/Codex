1ccbd9bf8a7aec636f54cf997dccd494afd8d06d
jcv8000 <jcv8000@gmail.com>
2021-04-07 00:34:53 -0500

    Change another version number

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ app.commandLine.appendSwitch("disable-http-cache");
 
 var mainWindow = null;
 
-const currentVersion = "1.2.1";
+const currentVersion = "1.2.2";
 
 
 const gotTheLock = app.requestSingleInstanceLock()
