03f542a4476f3c025f158a6810d5df7b12d25710
jcv8000 <jcv8000@gmail.com>
2021-06-13 23:39:47 -0500

    fix version number

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ app.commandLine.appendSwitch("disable-http-cache");
 
 var mainWindow = null;
 
-const currentVersion = "1.3.0";
+const currentVersion = "1.3.1";
 
 
 const gotTheLock = app.requestSingleInstanceLock()
