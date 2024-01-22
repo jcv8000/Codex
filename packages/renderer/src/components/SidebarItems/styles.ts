d9b25833c5fa7f6393b460927249bacb4abd73c7
Josh Vickery <jcv8000@gmail.com>
2024-01-22 13:16:28 -0600

    fix: more centralized sidebar item styles

--- packages/renderer/src/components/SidebarItems/styles.ts
+++ packages/renderer/src/components/SidebarItems/styles.ts
@@ -1,8 +1,8 @@
-import { rem } from "@mantine/core";
+import { FlexProps, rem } from "@mantine/core";
 
 export function getLeftPadding(depth: number) {
     return rem((depth + 1) * 12);
 }
 
-export const flexProps = { height: 34, gap: "sm", pr: "sm", align: "center" };
+export const flexProps: FlexProps = { h: 34, gap: "sm", pr: "sm", align: "center" };
 export const textSize = "sm";
