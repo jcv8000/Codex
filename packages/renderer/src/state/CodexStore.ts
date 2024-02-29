6bdcfc7f3eef8e9f8e309b8fb9a1ae67cc3ce2d3
Josh Vickery <jcv8000@gmail.com>
2024-02-29 15:18:50 -0600

    load/write save to disk

--- packages/renderer/src/state/CodexStore.ts
+++ packages/renderer/src/state/CodexStore.ts
@@ -1,13 +1,5 @@
 import { Prefs } from "common/schemas/v2/Prefs";
-import {
-    Folder,
-    NoteItem,
-    Page,
-    Save,
-    exampleSave,
-    getItemFromID,
-    getParent
-} from "common/schemas/v2/Save";
+import { Folder, NoteItem, Page, Save, getItemFromID, getParent } from "common/schemas/v2/Save";
 import { proxy, useSnapshot } from "valtio";
 import { Editor } from "@tiptap/react";
 import { locales } from "common/Locales";
@@ -28,7 +20,7 @@ type CodexStore = {
 };
 
 const startPrefs = JSON.parse(await window.ipc.invoke("get-prefs")) as Prefs;
-const startSave = exampleSave;
+const startSave = JSON.parse(await window.ipc.invoke("get-save")) as Save;
 
 export const codexStore = proxy<CodexStore>({
     view: { value: "home" },
@@ -43,15 +35,14 @@ export function saveActivePage() {}
 
 // export function exportPage(page: Page, type: "pdf" | "md") {}
 
-export async function modifyItem(id: string, changes: Partial<Folder> | Partial<Page>) {
+export function modifyItem(id: string, changes: Partial<Folder> | Partial<Page>) {
     const item = getItemFromID(codexStore.save, id);
     if (item != undefined) Object.assign(item, changes);
 
-    //const saved = await window.ipc.invoke("write-save", Save.stringify(codexStore.save));
-    // TODO show error notification if save isnt saved
+    writeSave();
 }
 
-export async function deleteItem(id: string) {
+export function deleteItem(id: string) {
     const parent = getParent(codexStore.save, id);
     if (parent == undefined) {
         // Top-level
@@ -63,9 +54,16 @@ export async function deleteItem(id: string) {
             if (c.id == id) parent.children.splice(index, 1);
         });
     }
+
+    writeSave();
 }
 
 export function useLocale() {
     const { prefs } = useSnapshot(codexStore);
     return locales[prefs.general.locale];
+}
+
+async function writeSave() {
+    const saved = await window.ipc.invoke("write-save", JSON.stringify(codexStore.save));
+    // TODO show error notification if save isnt saved
 }
