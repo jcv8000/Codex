de0a19dfccb184fc871102bfed0486fe13c39f0f
Josh Vickery <jcv8000@gmail.com>
2024-02-29 15:10:32 -0600

    sidebar floating New Folder btn

--- packages/renderer/src/components/Sidebar/index.tsx
+++ packages/renderer/src/components/Sidebar/index.tsx
@@ -1,7 +1,12 @@
+import { SidebarFloat } from "components/Floats";
 import { SidebarContent } from "./SidebarContent";
 import { SidebarResizer } from "./SidebarResizer";
+import { ActionIcon, Tooltip } from "@mantine/core";
+import { Icon } from "components/Icon";
+import { modalStore, useLocale } from "src/state";
 
 export function Sidebar() {
+    const locale = useLocale();
     return (
         <>
             <div id="sidebar">
@@ -9,6 +14,23 @@ export function Sidebar() {
             </div>
 
             <SidebarResizer />
+
+            <SidebarFloat pos={{ bottom: 16, right: 16 }}>
+                <Tooltip label={locale.sidebar.new_folder} withArrow position="left">
+                    <ActionIcon
+                        variant="default"
+                        onClick={() => {
+                            modalStore.newModalState = {
+                                opened: true,
+                                parent: null,
+                                type: "folder"
+                            };
+                        }}
+                    >
+                        <Icon icon="plus" />
+                    </ActionIcon>
+                </Tooltip>
+            </SidebarFloat>
         </>
     );
 }
