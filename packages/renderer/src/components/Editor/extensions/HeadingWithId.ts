9187175eb17c7b180c608a9a9183770365fab847
Josh Vickery <jcv8000@gmail.com>
2024-03-23 17:29:40 -0500

    add todo to HeadingWithId.ts

--- packages/renderer/src/components/Editor/extensions/HeadingWithId.ts
+++ packages/renderer/src/components/Editor/extensions/HeadingWithId.ts
@@ -1,5 +1,6 @@
 import { Heading } from "@tiptap/extension-heading";
 
+// TODO see if v0 saves' headers get id's generated or not
 export const HeadingWithId = Heading.extend({
     addAttributes() {
         return {
