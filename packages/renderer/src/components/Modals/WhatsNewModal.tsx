c8cd2b519ccc8c4361c0af675fc3cdf23eef6862
Josh Vickery <jcv8000@gmail.com>
2025-02-21 02:20:15 -0600

    WhatsNewModal: remove other sections

--- packages/renderer/src/components/Modals/WhatsNewModal.tsx
+++ packages/renderer/src/components/Modals/WhatsNewModal.tsx
@@ -30,87 +30,58 @@ export function WhatsNewModal(props: { state: { opened: boolean }; onClose: () =
             title={<Title order={3}>What&apos;s New in 2.0.5</Title>}
             size="lg"
         >
-            <Tabs orientation="vertical" placement="right" defaultValue="v205">
-                <Tabs.List>
-                    <Tabs.Tab value="v205">2.0.5 Changelog</Tabs.Tab>
-                    <Tabs.Tab value="bugs">Bug Reports</Tabs.Tab>
-                    <Tabs.Tab value="v2release">2.0 Changelog</Tabs.Tab>
-                </Tabs.List>
+            <Modal.Body>
+                <Title order={3}>*New Survey*</Title>
+                <Text>Please take this short survey about future versions of Codex:</Text>
+                <Anchor onClick={() => window.api.openLink("feedback")}>
+                    https://forms.gle/r4HqxLb6Yh663LV28
+                </Anchor>
 
-                <Tabs.Panel value="v205" px="xs">
-                    <Title order={3}>*New Survey*</Title>
-                    <Text>Please take this short survey about future versions of Codex:</Text>
+                <Space h="lg" />
+
+                <Title order={3}>General</Title>
+                <ul>
+                    <li>Updated to Electron 32 and Node.js 20</li>
+                    <li>Added Russian localization (@wennerryle)</li>
+                    <li>
+                        Updated MathLive to 0.104
+                        <ul>
+                            <li>
+                                This adds a new menu in the Math Editor to insert common math
+                                expressions easily, like a matrix.
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+
+                <Title order={3}>Bug Fixes</Title>
+                <ul>
+                    <li>Fixed an issue where favoriting / unfavoriting pages doesn&apos;t work.</li>
+                    <li>
+                        Fixed an issue where the &quot;textContent&quot; of notes was not being
+                        saved, which is what the search bar uses to find text.
+                        <br />
+                        <b>
+                            If the search bar is not correctly finding text in your notes, you might
+                            need to go through your pages manually and re-save them to generate each
+                            page&apos;s text content for searching.
+                        </b>
+                    </li>
+                </ul>
+
+                <Space h="lg" />
+
+                <Title order={3}>Other</Title>
+                <Text>
+                    Codex v3 (a full rewrite) is something that I would like to do soon, with a new
+                    website and docs. Currently waiting on TipTap v3 before I start again.{" "}
                     <Anchor onClick={() => window.api.openLink("feedback")}>
-                        https://forms.gle/r4HqxLb6Yh663LV28
-                    </Anchor>
-
-                    <Space h="lg" />
-
-                    <Title order={3}>General</Title>
-                    <ul>
-                        <li>Updated to Electron 32 and Node.js 20</li>
-                        <li>Added Russian localization (@wennerryle)</li>
-                        <li>
-                            Updated MathLive to 0.104
-                            <ul>
-                                <li>
-                                    This adds a new menu in the Math Editor to insert common math
-                                    expressions easily, like a matrix.
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-
-                    <Title order={3}>Bug Fixes</Title>
-                    <ul>
-                        <li>
-                            Fixed an issue where favoriting / unfavoriting pages doesn&apos;t work.
-                        </li>
-                        <li>
-                            Fixed an issue where the &quot;textContent&quot; of notes was not being
-                            saved, which is what the search bar uses to find text.
-                            <br />
-                            <b>
-                                If the search bar is not correctly finding text in your notes, you
-                                might need to go through your pages manually and re-save them to
-                                generate each page&apos;s text content for searching.
-                            </b>
-                        </li>
-                    </ul>
-
-                    <Space h="lg" />
-
-                    <Title order={3}>Other</Title>
-                    <Text>
-                        Codex v3 (a full rewrite) is something that I want to do soon, with a new
-                        website and docs. Currently waiting on TipTap v3 before I start again.{" "}
-                        <Anchor onClick={() => window.api.openLink("feedback")}>
-                            Take the new survey
-                        </Anchor>{" "}
-                        for suggestions and input. As always bug reports and issues go on the{" "}
-                        <Anchor onClick={() => window.api.openLink("issues")}>GitHub</Anchor>.
-                    </Text>
-                </Tabs.Panel>
-
-                <Tabs.Panel value="bugs" px="xs">
-                    If you find any bugs/issues with the new release of Codex, create an Issue on
-                    the GitHub Repository{" "}
-                    <Anchor onClick={() => window.api.openLink("issues")}>here</Anchor>.
-                    <Space h="md" />
-                    Please include your platform (Windows, Linux, macOS) and the version of Codex
-                    that you&apos;re running.
-                    <Space h="md" />
-                    There&apos;s also a new feedback survey{" "}
-                    <Anchor onClick={() => window.api.openLink("feedback")}>here</Anchor> for
-                    suggestions, and a question about Windows 7/8/8.1 support.
-                </Tabs.Panel>
-
-                <Tabs.Panel value="v2release" px="xs">
-                    <Anchor onClick={() => window.api.openLink("changelogs")}>
-                        Codex 2.0.0 Changelogs
-                    </Anchor>
-                </Tabs.Panel>
-            </Tabs>
+                        Take the new survey
+                    </Anchor>{" "}
+                    for suggestions and input. As always bug reports and issues go on the{" "}
+                    <Anchor onClick={() => window.api.openLink("issues")}>GitHub</Anchor>.
+                </Text>
+            </Modal.Body>
         </Modal>
     );
 }
